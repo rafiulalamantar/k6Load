f5841dbb8eb7a6686dc1d150ced929e286001226
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-03-18 13:09:27 +0600

    Add pizza response time tracking and update thresholds

--- test2.js
+++ test2.js
@@ -1,6 +1,9 @@
 
 import http from "k6/http"
 import { check, sleep } from "k6";
+import {Trend} from "k6/metrics";
+
+const aoiResponseTime = new Trend('pizza_response_time');
 
 export const options = {
 
@@ -14,13 +17,23 @@ export const options = {
         http_req_duration: ['p(95)<500'], // 95% of requests should be below 500ms
         http_req_failed: ['rate<0.01'], // less than 1% of requests should fail
         'checks': ['rate>0.9'],    // 90% of checks should pass
-        'http_req_duration{name: pizza-api}': ['p(95)<500']
+        'http_req_duration{name: pizza-api}': ['p(95)<500'],
+        'pizza_response_time': ['p(95)<200']
 
     }
 }
 
 export default function () {
     const res = http.get("https://quickpizza.grafana.com");
+
+    pizza_response_time.add(res.timings.waiting);
+    //DNS Lookup time
+    //TCP Connection time
+    //TLS Handshake time
+    //Time to First Byte
+    //Content Download time
+    //Total time taken for the request to complete
+
     check(res, {
         'is status 200': (r) => r.status === 200,
         'page contains pizza': (r) => {

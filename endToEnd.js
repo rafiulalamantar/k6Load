4a77bfe7e37ea19bf86795c9af323b4c96c700e6
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-03-19 11:15:36 +0600

    Refactor user registration and login flow into separate groups for improved readability and structure

--- endToEnd.js
+++ endToEnd.js
@@ -1,44 +1,73 @@
-
-import { check } from 'k6';
+import { check, group } from 'k6';
 import http from 'k6/http';
 import { sleep } from 'k6';
 
 const BASE_URL = 'https://quickpizza.grafana.com';
-const USERNAME= "Antar"
-const PASSWORD= "12345678"
+const USERNAME = "Antar";
+const PASSWORD = "12345678";
 
 export const options = {
     vus: 1,
     duration: '3s',
-
-
-}
+};
 
 export default function () {
 
     let userRegistered = false;
-    const registerPayLoad={
-        username: USERNAME,
-        password: PASSWORD
-    }
-    const params = {
-        headers: {
-            'Content-Type': 'application/json'
+
+    group('User Registration', function () {
+
+        const registerPayLoad = {
+            username: USERNAME,
+            password: PASSWORD
+        };
+
+        const params = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const response = http.post(`${BASE_URL}/api/users`, JSON.stringify(registerPayLoad), params);
+        console.log(response.body);
+
+        userRegistered = check(response, {
+            'is status 201': (r) => r.status === 201
+        });
+
+        if (!userRegistered) {
+            console.error(`User registration failed. ${response.status}-${response.body}`);
         }
-    }
-    const response = http.post(`${BASE_URL}/api/users`, JSON.stringify(registerPayLoad),params);
-    console.log(response.body);
-    userRegistered = check(response, {
-        'is status 201': (r) => {
-            return response.status === 201
-        }
-    
+
+        sleep(1);
     });
 
-    if(!userRegistered){
-        console.error(`User registration failed. ${response.status}-${response.body}`);
-    }
-    sleep(1);
+    group('User Login', function () {
 
+        let userAuthenticated = false;
 
+        const loginPayLoad = {
+            username: USERNAME,
+            password: PASSWORD
+        };
+
+        const params = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const response = http.post(`${BASE_URL}/api/users/token/login`, JSON.stringify(loginPayLoad), params);
+        console.log(response.body);
+
+        userAuthenticated = check(response, {
+            'is status 200': (r) => r.status === 200
+        });
+
+        if (!userAuthenticated) {
+            console.error(`Login failed. ${response.status}-${response.body}`);
+        }
+
+        sleep(1);
+    });
 }

0f4b0010bb5abf474698c1be77f5d2385b81de9c
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-03-20 10:50:30 +0600

    Refactor browser test implementation to include scenario options and enhance page navigation checks

--- browserTest.js
+++ browserTest.js
@@ -1,4 +1,30 @@
+import { type } from "wd/lib/commands";
 import browser from k6 / browser;
+
+export const options = {
+    scenarios: {
+        ui: {
+            executor: 'shared-iterations',
+            exec: "browserTest",
+            vus: 2,
+            maxDuration: '1m',
+            iterations: 3,
+            options: {
+                browser: {
+                    headless: true,
+                    type: "chromium",
+                }
+
+            },
+            backend: {
+                exec: "backendTest",
+                vus: 1,
+                duration: '1m',
+
+            }
+        }
+    }
+}
 
 export async function browserTest() {
 
@@ -12,11 +38,17 @@ export async function browserTest() {
 
     await page.waitForTimeout(2000);
 
-    check(res, {
-        'is status 200': (r) => r.status === 200,
-        'page contains pizza': (r) => {
+    const pageTitle = await page.locator("h1").first().textContent();
 
-            r.body.includes("pizza")
+    check(pageTitle, {
+
+        header: (text) => {
+            return text.includes("Welcome")
         }
     });
+
+}
+
+export async function backendTest() {
+
 }
